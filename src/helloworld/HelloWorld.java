9502df52735cbb1b2f7d8d36120cae476932c39a
oweson <@oweson570347720qq.com>
2018-03-25 21:18:03 +0800

    只是随便测试一下

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("hello,world");
+		System.err.println("随便改一改！！！");
 		// TODO Auto-generated method stub
 
 	}

20cfba270559f3cae7cea1cfe6ff49f3687bc7ba
oweson <570347720@qq.com>
2018-06-13 11:31:56 +0800

    添加author
    添加了主人公；

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		int a=1000000;
 		int sum=210000;
+		System.out.println("hello万里");
 		System.out.println("hello,world");
 		System.err.println("随便改一改！！！");
 		// TODO Auto-generated method stub

673b26967a33900a860e682c43f8c057d5bea57d
oweson <570347720@qq.com>
2018-06-13 11:28:51 +0800

    add
    添加新的变量

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,6 +3,8 @@ package helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		int a=1000000;
+		int sum=210000;
 		System.out.println("hello,world");
 		System.err.println("随便改一改！！！");
 		// TODO Auto-generated method stub

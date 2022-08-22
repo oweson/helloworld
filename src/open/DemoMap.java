849e0a9a8c24fe0a6dc13694950f434f5d434dd2
oweson <570347720@qq.com>
2022-08-22 23:07:36 +0800

    feature ok!

--- src/open/DemoMap.java
+++ src/open/DemoMap.java
@@ -1,5 +1,7 @@
 package open;
 
+import java.util.ArrayList;
+
 /**
  * @author oweson
  * @date 2022/8/22 22:53
@@ -9,5 +11,7 @@ package open;
 public class DemoMap {
     public static void main(String[] args) {
         System.out.println("main-20220822");
+        System.out.println("feature ok!");
+        ArrayList<Object> objects = new ArrayList<>();
     }
 }
